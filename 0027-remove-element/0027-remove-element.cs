b6938d18a1fc57252479658ee900a7aecbaeda7c
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2025-09-07 10:26:20 +0500

    initial

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var result = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[result] = nums[i];
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
