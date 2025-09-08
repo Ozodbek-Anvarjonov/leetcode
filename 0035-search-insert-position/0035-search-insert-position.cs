d661fe52d89bfa6c31c1dc9676c5942cd3474278
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2025-09-08 11:26:47 +0500

    0035

--- 0035-search-insert-position/0035-search-insert-position.cs
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -2,16 +2,12 @@ public class Solution
 {
     public int SearchInsert(int[] nums, int target)
     {
-        var index = 0;
-
         for (int i = 0; i < nums.Length; i++)
         {
             if (target <= nums[i])
                 return i;
-
-            index = i;
         }
 
-        return index + 1;
+        return nums.Length;
     }
 }
